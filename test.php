798d05c98e0d7502d058b4f5e700713475b0dd58
lilipbb <542896590@qq.com>
2018-11-01 22:52:47 +0800

    测试文件

---
+++ test.php
@@ -0,0 +1,3 @@
+<?php
+include "php2zephir.php";
+\ZephirHelp\Php2Zephir::ChangeDir("testphp","testphp",-1);
---
+++ testphp/1.php
@@ -0,0 +1,9 @@
+<?php
+namespace Test{
+    use PbbHelpers\Util;
+    class Test{
+        public static function run(){
+            print(111);
+        }
+    }
+}
