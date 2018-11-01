7472c41e527a10d39552315bf4d65a26ac89b9d0
lilipbb <542896590@qq.com>
2018-11-02 00:09:05 +0800

    测试文件

--- test.php
+++ test.php
@@ -1,3 +1,3 @@
 <?php
 include "php2zephir.php";
-\ZephirHelp\Php2Zephir::ChangeDir("testphp","testphp",-1);
+\Php2Zephir\ChangeDir("testphp","testphp",0);
--- testphp/test.php
+++ testphp/test.php
@@ -1,8 +1,28 @@
 <?php
 namespace Test{
     class Test{
-        public static function run(){
-            print(111);
+        //字符串测试
+        public static function StringTest(){
+            $a="hello";
+            $b="world";
+            print("{$a}${b}"."!");
+            $c=<<<Str
+            <span>{$a}{$b}<span>
+Str;
+            echo $c;
+        }
+        //内部函数测试
+        public static function InlineFunTest(){
+            $fun=function (){ echo("fun1"); };
+            echo($fun());
+        }
+        //内部函数测试
+        public static function InlineFunTest1(){
+            $a=1;
+            $b=2;
+            $fun=function()use($a,$b){ echo($a+$b); };
+            $fun();
+            
         }
     }
 }
